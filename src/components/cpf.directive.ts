5b8f45a050664571933123c7ce0ff690ca52982f
Eder Ribeiro <eder@ederibeiro.com>
2017-03-21 16:44:03 -0300

    Adding the directive

---
+++ src/components/cpf.directive.ts
@@ -0,0 +1,88 @@
+import { Directive, forwardRef, Attribute } from '@angular/core';
+import { NG_VALIDATORS, AbstractControl, Validator } from '@angular/forms';
+
+function validateCpfFactory() {
+    return (c: AbstractControl) => {
+        let cpf = c.value,
+            numbers,
+            digits,
+            sum,
+            result,
+            equalDigits = true;
+
+        if (cpf.length < 11 || cpf.length > 11) {
+            return {
+                validateCpf: {
+                    valid: false
+                }
+            };
+        }
+
+        for (let i = 0; i < cpf.length - 1; i++) {
+            if (cpf.charAt(i) != cpf.charAt(i + 1)) {
+                equalDigits = false;
+                break;
+            }
+        }
+
+        if (equalDigits === false) {
+            numbers = cpf.substring(0, 9);
+            digits = cpf.substring(9);
+            sum = 0;
+            for (let i = 10; i > 1; i--) {
+                sum += numbers.charAt(10 - i) * i;
+            }
+
+            result = sum % 11 < 2 ? 0 : 11 - sum % 11;
+            if (result != digits.charAt(0)) {
+                return {
+                    validateCpf: {
+                        valid: false
+                    }
+                };
+            }
+
+            numbers = cpf.substring(0, 10);
+            sum = 0;
+            for (let i = 11; i > 1; i--) {
+                sum += numbers.charAt(11 - i) * i;
+            }
+            result = sum % 11 < 2 ? 0 : 11 - sum % 11;
+            if (result != digits.charAt(1)) {
+                return {
+                    validateCpf: {
+                        valid: false
+                    }
+                };
+            }
+
+            return null;
+        }
+
+        return {
+            validateCpf: {
+                valid: false
+            }
+        };
+    }
+}
+
+@Directive({
+  selector: '[validateCpf][ngModel],[validateCpf][formControl],[validateCpf][formControlName]',
+  providers: [
+      { provide: NG_VALIDATORS, useExisting: forwardRef(() => CpfValidator), multi:true }
+  ]
+})
+export class CpfValidator implements Validator {
+
+    validator: Function;
+
+    constructor(@Attribute('validateCpf') public validateCpf: string) {
+        this.validator = validateCpfFactory();
+    }
+
+    validate(c: AbstractControl): { [key: string]: any } {
+        return this.validator(c);
+    }
+}
+
